2a201fbf79ae3d500d2d1ae486c243245b1b0b1d
kitet <kitetnet@gmail.com>
2017-07-20 11:32:15 +0300

    added post request to handle form submission while creating new company and subscriber

--- app.rb
+++ app.rb
@@ -9,3 +9,19 @@ get '/' do
     @subscribers = Subscriber.all
     erb :index
 end
+
+post '/index/company/new' do
+    @company_name=params.fetch('name')
+    @company_email=params.fetch('email')
+    Company.create(name: @company_name, email: @company_email)
+    redirect '/'
+end
+
+
+post '/index/subscriber/new' do
+    @subscriber_name=params.fetch('name')
+    @subscriber_email=params.fetch('email')
+    Subscriber.create(name:@subscriber_name, email:@subscriber_email)
+    redirect '/'
+end
+
